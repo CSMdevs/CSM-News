e1d5506a72d18b32133b36cdd61d173c71ef94c1
Stan Verlaan <s.verlaan@ziggo.nl>
2019-11-18 18:24:17 +0100

    Update get_home.py

--- get_home.py
+++ get_home.py
@@ -1,6 +1,11 @@
-import urllib.request
+import requests
 import json
 
 # The JSON
-contents = urllib.request.urlopen("https://newsapi.org/v2/top-headlines?country=nl&apiKey=4898a7ecddb347c8b3998f1146d7b7a9").read()
+contents = requests.get("https://newsapi.org/v2/top-headlines?country=nl&apiKey=4898a7ecddb347c8b3998f1146d7b7a9").json()
 
+status = contents['status']
+totalResults = contents['totalResults']
+
+print('API status: ' + status)
+print('Total results: ' + str(totalResults))
