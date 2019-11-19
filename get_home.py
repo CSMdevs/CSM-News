b88b1d1e0d9d9f8855e5487e544377acee9edae7
Stan Verlaan <s.verlaan@ziggo.nl>
2019-11-19 22:08:16 +0100

    Updated Python
    The python script can now display all the wanted contents

--- get_home.py
+++ get_home.py
@@ -6,8 +6,19 @@ contents = requests.get("https://newsapi.org/v2/top-headlines?country=nl&apiKey=
 
 status = contents['status']
 totalResults = contents['totalResults']
-source1 = contents['source']
+source1 = contents['articles'][0]['source']['name']
 
-print('API status: ' + status)
-print('Total results: ' + str(totalResults))
+print(status)
+print(totalResults)
 print(source1)
+
+def my_function(contents):
+    for x in contents['articles']:
+        print("Source: " + str(x['source']['name']))
+        print("Author: " + str(x['author']))
+        print("Title: " + str(x['title']))
+        print("Description: " + str(x['description']))
+        print("Url: " + str(x['url']))
+        print("Url: " + str(x['url']))
+
+my_function(contents)
