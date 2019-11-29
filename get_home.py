5ff7dbb348042c41c6e1065436ecf7693954c778
Stan Verlaan <s.verlaan@ziggo.nl>
2019-11-29 09:54:53 +0100

    Python script

--- get_home.py
+++ get_home.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 
 # The JSON
 from newsapi import NewsApiClient
@@ -8,17 +9,17 @@ from newsapi import NewsApiClient
 newsapi = NewsApiClient(api_key='4898a7ecddb347c8b3998f1146d7b7a9')
 
 # /v2/top-headlines
-def function(q=None, sources=None, category=None, language=None, country=None):
+def function(category=None, country=None, domains=None, from_param=datetime.date(2019, 11, 21), language=None, page=None, pageSize=None, q=None, sort_by=None, sources=None, to=None):
     top_headlines = newsapi.get_top_headlines(category=category,
-                                          country=country,                                      
-                                          domains=domains,
+                                          country=country,
                                           from_param=from_param,
                                           language=language,
-                                          page=2,
+                                          page=page,
+                                          pageSize=pageSize,
                                           q=q,
-                                          sort_by=sort_by
+                                          sort_by=sort_by,
                                           sources=sources,
                                           to=to)
     return top_headlines
 
-print(function(q='bitcoin'))
+print(function())
